da1623e8299fca6e7d7ed1dccd46ef3dc34410ee
mariamav <avanessovam@gmail.com>
2022-03-03 16:52:06 +0100

    docs: documentation du fichier principal

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
+"""
+    Projet de démo pour la création d'un CD
+"""
 from flask import Flask
 import os
 
+# Récupération des ENVs
 HOST = os.getenv("HOST", "0.0.0.0")
 PORT = os.getenv("PORT", "80")
 app = Flask("demo")
 
 @app.get("/")
 def hello():
+    """Foncton appelée pour le chemin par défaut
+
+    Returns:
+        str: un message de la plus grande importance
+    """
     return "Hello World !!!"
 
 if __name__ == "__main__":
