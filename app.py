9c14cdf5d499a31e310631438bd675ad9b58110d
mariamav <avanessovam@gmail.com>
2022-03-03 16:42:53 +0100

    feat: ajout de la route pour l'affichage du message 'Hello World'

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+
+HOST = os.getenv("HOST", "0.0.0.0")
+PORT = os.getenv("PORT", "80")
+app = Flask("demo")
+
+@app.get("/")
+def hello():
+    return "Hello World"
+
+if __name__ == "__main__":
+    app.run(host=HOST, port=PORT)
+    
+    
+    
